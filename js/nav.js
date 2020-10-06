6507cd025bc8f851c0e2f734cd923e85919aac13
Alana Cargill <alanalouisecargill@gmail.com>
2020-10-06 21:44:40 +0100

    week two day one

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,7 @@
-var menuRight = document.getElementById( 'slide-menu' );
+var menuRight = document.querySelector( '.menu' );
 var showMenu = document.getElementById( 'show-menu' );
 var hideMenu = document.getElementById('hide-menu');
-var navElements = document.querySelectorAll('arrow-anchor');
-var arrowIcon = document.querySelectorAll('arrow-icon');
-    
+
 showMenu.addEventListener('click', (e) => {
     menuRight.classList.toggle('menu-open');
 });
